674e80736257f0cdb958079d630b2716428442f6
wallchs <wallace.s.chagas@gmail.com>
2025-04-02 03:22:46 -0300

    Incluindo tailwindcss para testes e adicionando novas pastas

---
+++ src/components/Heading.tsx
@@ -0,0 +1,12 @@
+import "../styles/heading.css"
+export function Heading() {
+  console.log('Oi mundo novamente');
+
+  return (
+    <>
+      <p> Aqui está o Heading</p>
+      
+    </>
+  )
+  
+}
