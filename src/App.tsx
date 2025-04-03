94a7ff0d8203a58989d975ba3d061a495b35e3e3
wallchs <wallace.s.chagas@gmail.com>
2025-04-03 07:30:07 -0300

    Incluindo bulma no projeto

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ function App() {
   return (
     <>
       <Heading />
-      <div >
-        <p className='grid justify-items-center '>um novo texto aqui!</p>
+      <div className='is-flex is-justify-content-center'>
+        <p>um novo texto aqui!</p>
       </div>
     </>
   );
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -3,7 +3,7 @@ export function Heading() {
 
     return (
     <>
-      <h1 className='txt-9xl'>Aqui o Heading</h1>
+      <h1>Aqui o Heading</h1>
       
     </>
   );
