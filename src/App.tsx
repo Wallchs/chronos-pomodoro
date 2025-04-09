b6e3ceee9f216a353fcdd99c7ec85392e8d169b3
wallchs <wallace.s.chagas@gmail.com>
2025-04-09 02:57:17 -0300

    Ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import { TimerIcon } from 'lucide-react';
-import { Heading } from './components/Heading.tsx';
+import { TimerIcon } from "lucide-react";
+import { Heading } from "./components/Heading.tsx";
 function App() {
-
-  return (
-    <>
-      <Heading>
-        <button className="p-auto m-auto">
-          <TimerIcon />
-        </button>
-      </Heading>
-      <div className='is-flex is-justify-content-center'>
-        <p>um novo texto aqui!</p>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<Heading>
+				<button className="p-auto m-auto">
+					<TimerIcon />
+				</button>
+			</Heading>
+			<div className="is-flex is-justify-content-center">
+				<p>um novo texto aqui!</p>
+			</div>
+		</>
+	);
 }
 
 export default App;
